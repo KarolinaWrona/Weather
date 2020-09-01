ecef01fe8068e655a427c5966c6d7019171bf0a7
KarolinaWrona <karolinawrona9804@gmail.com>
2020-09-01 20:03:23 +0200

    Updates

---
+++ precache-manifest.f0a599166478d346e94022b2363e0fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20bf3992c22d2513a6affb42d97547f",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6e655563f0a4d04e9de0",
+    "url": "/weather/static/css/main.befc8de9.chunk.css"
+  },
+  {
+    "revision": "494dadf966bebacc49f5",
+    "url": "/weather/static/js/2.94e66824.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather/static/js/2.94e66824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e655563f0a4d04e9de0",
+    "url": "/weather/static/js/main.a6b1f0cd.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "1d8bf7399ae0fdb549cdf7840b1766fd",
+    "url": "/weather/static/media/aurora.1d8bf739.jpg"
+  }
+]);
